f5e614486f268f68051482745e84495ded1648da
Ivan Semkin <ivan@semkin.ru>
2024-11-27 23:07:53 +0300

    Add writing redirections to file

--- upnp-monitor/app.py
+++ upnp-monitor/app.py
@@ -18,10 +18,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
+import json
+import sys
 from time import sleep
 
 import miniupnpc
-import sys
 
 
 class UPnPMonitor(object):
@@ -81,18 +82,65 @@ class UPnPMonitor(object):
         return redirections
 
 
+def print_formatted_redirection(redirection) -> str:
+    (protocol,
+     host_ip, host_port,
+     remote_host, remote_port,
+     description) = redirection
+    text_template = '{protocol} {host_ip}:{host_port} => {destination_ip}:{destination_port} as "{description}"'
+    return text_template.format(
+        protocol=protocol,
+        host_ip=host_ip, host_port=host_port,
+        destination_ip=remote_host, destination_port=remote_port,
+        description=description)
+
+
 def main():
     upnp_monitor = UPnPMonitor()
     upnp_monitor.initialize()
     upnp_monitor.discover_igd()
     upnp_monitor.select_igd()
-    all_redirections = []
+
+    historical_redirections = []
+    filename = 'redirections.txt'
+    try:
+        with open(filename, 'r') as json_file:
+            for json_line in json_file:
+                historical_redirections.append(json.loads(json_line))
+        for historical_redirection in historical_redirections:
+            print(print_formatted_redirection(historical_redirection))
+    except:
+        print('Existing redirections.txt file not found')
+
     while True:
         current_redirections = upnp_monitor.list_redirections()
-        for redirection in current_redirections:
-            if not all_redirections.__contains__(redirection):
-                all_redirections.append(redirection)
-                print(redirection)
+        for current_redirection in current_redirections:
+            if len(historical_redirections) == 0:
+                historical_redirections.append(current_redirection)
+                continue
+
+            already_recorded = False
+
+            (current_protocol,
+             current_host_ip, current_host_port,
+             current_remote_host, current_remote_port,
+             current_description) = current_redirection
+            for historical_redirection in historical_redirections:
+                (historical_protocol,
+                 historical_host_ip, historical_host_port,
+                 historical_remote_host, historical_remote_port,
+                 historical_description) = historical_redirection
+                if (current_protocol == historical_protocol and
+                        current_host_ip == historical_host_ip and current_host_port == historical_host_port and
+                        current_remote_host == historical_remote_host and current_remote_port == historical_remote_port):
+                    already_recorded = True
+
+            if already_recorded is False:
+                print(print_formatted_redirection(current_redirection))
+                historical_redirections.append(current_redirection)
+                with open(filename, 'a') as file:
+                    json_string = json.dumps(current_redirection)
+                    file.write(json_string + '\n')
         sleep(2)
 
 
